b9a0c973904c5e709374d8b95fc1fb4eda3523bd
Richard Macdonald <macdrich@gmail.com>
2022-09-25 13:13:37 -0600

    adds frontend branding, amplify config

---
+++ notes/frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+
+import './App.css';
+import Routes from "./Routes";
+
+function App() {
+  return (
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Uber Notes
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
